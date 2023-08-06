da506c98cdbe01b0063ee493883e00db9cf8ee9b
01xz <73350430+01xz@users.noreply.github.com>
2023-08-06 20:28:44 +0800

    feat: butterfly

---
+++ src/main/scala/arithmetic/butterfly/Butterfly.scala
@@ -0,0 +1,34 @@
+package arithmetic.butterfly
+
+import chisel3._
+import chisel3.util._
+
+// Butterfly Network: https://en.wikipedia.org/wiki/Butterfly_network
+trait Butterfly[T <: Data] {
+  val op: (T, T) => (T, T)
+
+  private def layer(rank: Int, in: Seq[T]): Seq[T] = {
+    require(rank <= log2Up(in.length))
+    require(isPow2(in.length))
+
+    val next = in
+      .grouped(in.length >> (rank - 1))
+      .map { s =>
+        val ss       = s.splitAt(s.length / 2)
+        val (lo, hi) = (ss._1 zip ss._2).map { case (l, h) => op(l, h) }.unzip
+        lo ++ hi
+      }
+      .flatten
+      .toSeq
+
+    if (rank < log2Up(in.length))
+      layer(rank + 1, next)
+    else
+      next
+  }
+
+  def butterfly(in: Seq[T]): Seq[T] = {
+    require(isPow2(in.length))
+    layer(1, in)
+  }
+}
