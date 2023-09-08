9bfb7e05902af03f2120e5b1fb297d04259cec15
01xz <73350430+01xz@users.noreply.github.com>
2023-09-08 21:48:56 +0800

    feat: Wallace tree

---
+++ src/main/scala/arithmetic/compressor/WallaceTree.scala
@@ -0,0 +1,43 @@
+package arithmetic.compressor
+
+import chisel3._
+import chisel3.util._
+import scala.collection.mutable.Buffer
+
+trait WallaceTree {
+  val original: Seq[Seq[Bool]]
+
+  def c32(in: Seq[Bool]): Seq[Bool]
+  def c22(in: Seq[Bool]): Seq[Bool]
+
+  def compress(cur: Seq[Seq[Bool]]): Seq[Seq[Bool]] = {
+    val rows = cur.map(_.length).max
+    if (rows == 2)
+      cur
+    else {
+      val next = Buffer.fill(original.size)(Buffer[Bool]())
+      cur.zipWithIndex.filter(_._1.size > 0).foreach { case (col, i) =>
+        val (toCompress, remaining) = col.grouped(3).toSeq.partition(_.size == 3)
+        val compressed = toCompress.map { case Seq(a, b, c) =>
+          c32(Seq(a, b, c))
+        }
+        val remained = remaining.headOption.getOrElse(Seq.empty).toSeq
+        compressed.foreach { case Seq(sum, carry) =>
+          next(i) += sum
+          next(i + 1) += carry
+        }
+        if (rows == 3 && remained.size == 2 && next(i).size > 0) {
+          val Seq(sum, carry) = c22(remained)
+          next(i) += sum
+          next(i + 1) += carry
+        } else next(i) ++= remained
+      }
+      compress(next.map(_.toSeq).toSeq)
+    }
+  }
+
+  def getTwoOperands(): Vec[UInt] = VecInit {
+    val padded = compress(original).map(_.padTo(2, false.B))
+    padded.transpose.map(_.reverse).map(Cat(_))
+  }
+}
