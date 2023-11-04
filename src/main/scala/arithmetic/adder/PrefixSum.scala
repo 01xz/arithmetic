ceb51a203acfceef42817a89e18eef757f67b03c
01xz <73350430+01xz@users.noreply.github.com>
2023-11-04 20:13:54 +0800

    refactor(adder): minimize the usage of `else`

--- src/main/scala/arithmetic/adder/PrefixSum.scala
+++ src/main/scala/arithmetic/adder/PrefixSum.scala
@@ -11,35 +11,29 @@ trait PrefixSum {
 
 trait HanCarlsonPrefixSum extends PrefixSum {
   private def layer(elements: Seq[(Bool, Bool)], offset: Int): Seq[(Bool, Bool)] = {
+    val maxOffset = log2Up(elements.size) + 1
     require((elements.size & 1) == 0)
-    require(offset <= log2Up(elements.size) + 1)
+    require(offset <= maxOffset)
 
     val next = Seq.tabulate(elements.size) { i =>
-      if (offset == 1) { // Brent-Kung
-        if ((i & 1) == 1) {
+      offset match {
+        case 1 if (i & 1) == 1 => // Brent-Kung
           prefix(s"brent_kung_${offset}_${i}_${i - 1}") {
             associativeOp(Seq(elements(i - 1), elements(i)))
           }
-        } else {
-          elements(i)
-        }
-      } else if (offset == log2Up(elements.size) + 1) { // Brent-Kung
-        if (i > 1 && (i & 1) == 0) {
+        case maxOffset if i > 1 && (i & 1) == 0 => // Brent-Kung
           prefix(s"brent_kung_${offset}_${i}_${i - 1}") {
             associativeOp(Seq(elements(i - 1), elements(i)))
           }
-        } else {
-          elements(i)
-        }
-      } else { // Kogge-Stone
-        val n = 1 << (offset - 1)
-        if ((i & 1) == 1 && i > n) {
-          prefix(s"kogge_stone_${offset}_${i}_${i - n}") {
-            associativeOp(Seq(elements(i - n), elements(i)))
+        case _ =>
+          val n = 1 << (offset - 1)
+          if ((i & 1) == 1 && i > n) { // Kogge-Stone
+            prefix(s"kogge_stone_${offset}_${i}_${i - n}") {
+              associativeOp(Seq(elements(i - n), elements(i)))
+            }
+          } else {
+            elements(i)
           }
-        } else {
-          elements(i)
-        }
       }
     }
 
