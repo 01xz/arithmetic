1cf72217154497c680c13fe484c37ddbb780ef6a
01xz <73350430+01xz@users.noreply.github.com>
2023-11-04 20:51:12 +0800

    feat(adder): Brent Kung prefix sum

--- src/main/scala/arithmetic/adder/PrefixSum.scala
+++ src/main/scala/arithmetic/adder/PrefixSum.scala
@@ -60,6 +60,43 @@ trait KoggeStonePrefixSum extends PrefixSum {
   def getPrefixSum() = layer(original, 1)
 }
 
+trait BrentKungPrefixSum extends PrefixSum {
+  private def layer(elements: Seq[(Bool, Bool)], offset: Int): Seq[(Bool, Bool)] = {
+    val maxOffset = log2Up(elements.size) * 2 - 1
+    require((elements.size & 1) == 0)
+    require(offset <= maxOffset)
+
+    val next = Seq.tabulate(elements.size) { i =>
+      if (offset > log2Up(elements.size)) {
+        val n = log2Up(elements.size) * 2 - offset
+        val m = (1 << n) - 1
+        if (i > m && ((i - (1 << (n - 1))) & m) == m) {
+          val f = i - (1 << (n - 1))
+          associativeOp(Seq(elements(f), elements(i)))
+        } else {
+          elements(i)
+        }
+      } else {
+        val m = (1 << offset) - 1
+        if ((i & m) == m) {
+          val f = i - (1 << (offset - 1))
+          associativeOp(Seq(elements(f), elements(i)))
+        } else {
+          elements(i)
+        }
+      }
+    }
+
+    if (offset < maxOffset) {
+      layer(next, offset + 1)
+    } else {
+      next
+    }
+  }
+
+  def getPrefixSum() = layer(original, 1)
+}
+
 trait HanCarlsonPrefixSum extends PrefixSum {
   private def layer(elements: Seq[(Bool, Bool)], offset: Int): Seq[(Bool, Bool)] = {
     val maxOffset = log2Up(elements.size) + 1
