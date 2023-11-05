10228a72b330652d26e4360d2264c29801f4ec62
01xz <73350430+01xz@users.noreply.github.com>
2023-11-05 21:09:55 +0800

    chore: code format

--- src/main/scala/arithmetic/adder/Adder.scala
+++ src/main/scala/arithmetic/adder/Adder.scala
@@ -3,7 +3,7 @@ package arithmetic.adder
 import chisel3._
 
 abstract class Adder(width: Int) extends Module {
-  val io = IO(new Bundle{
+  val io = IO(new Bundle {
     val lhs, rhs = Input(UInt(width.W))
     val cin      = Input(Bool())
     val sum      = Output(UInt(width.W))
