83a623400158d8e4b0be80f0d3bb95fc2cb49043
01xz <73350430+01xz@users.noreply.github.com>
2023-11-08 20:47:19 +0800

    refactor(butterfly): non-recursive butterfly network

--- arithmetic/src/butterfly/Butterfly.scala
+++ arithmetic/src/butterfly/Butterfly.scala
@@ -1,34 +1,28 @@
 package arithmetic.butterfly
 
 import chisel3._
-import chisel3.util._
+import chisel3.util.{isPow2, log2Up}
 
-// Butterfly Network: https://en.wikipedia.org/wiki/Butterfly_network
+/** A trait representing a butterfly network.
+  * Butterfly Network: https://en.wikipedia.org/wiki/Butterfly_network
+  */
 trait Butterfly[T <: Data] {
   def op(in: (T, T)): (T, T)
 
-  private def layer(rank: Int, in: Seq[T]): Seq[T] = {
-    require(rank <= log2Up(in.length))
-    require(isPow2(in.length))
-
-    val next = in
-      .grouped(in.length >> (rank - 1))
-      .map { s =>
-        val ss       = s.splitAt(s.length / 2)
-        val (lo, hi) = (ss._1 zip ss._2).map(op(_)).unzip
-        lo ++ hi
-      }
-      .flatten
-      .toSeq
-
-    if (rank < log2Up(in.length))
-      layer(rank + 1, next)
-    else
-      next
-  }
-
   def butterfly(in: Seq[T]): Seq[T] = {
     require(isPow2(in.length))
-    layer(1, in)
+
+    (1 to log2Up(in.length)).foldLeft(in) { case (prev, rank) =>
+      prev
+        .grouped(prev.length >> (rank - 1))
+        .map { group =>
+          val (lo, hi) = group.splitAt(group.length / 2) match {
+            case (l, r) => (l zip r).map(op).unzip
+          }
+          lo ++ hi
+        }
+        .flatten
+        .toSeq
+    }
   }
 }
