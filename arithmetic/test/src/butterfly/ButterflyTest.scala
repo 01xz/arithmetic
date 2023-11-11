d64ff218a51b8e82218b40e739a8617653182439
01xz <73350430+01xz@users.noreply.github.com>
2023-11-11 14:14:10 +0800

    test(butterfly): formal verification tester of butterfly

--- arithmetic/test/src/butterfly/ButterflyTest.scala
+++ arithmetic/test/src/butterfly/ButterflyTest.scala
@@ -1,3 +1,26 @@
-package arithmetic.test.butterfly
+package arithmetic.butterfly
 
-object ButterflyTest {}
+import chisel3._
+import arithmetic.formal._
+import chiseltest.ChiselScalatestTester
+import chiseltest.formal._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class ButterflyTest extends AnyFlatSpec with ChiselScalatestTester with Formal with FormalBackendOption {
+  "Test Butterfly 4" should "PASS" in {
+    val a, b, c, d = UInt(8.W)
+    verifyButterflyWith(Seq(a, b, c, d)) { output =>
+      assert(output === Seq(d, c, b, a))
+    }
+  }
+
+  private def verifyButterflyWith[T <: Data](input: Seq[T])(verifyFn: Seq[T] => Unit): Unit = {
+    verify(
+      new Module {
+        val output = Butterfly(input)(_.swap)
+        verifyFn(output)
+      },
+      Seq(BoundedCheck(1), DefaultBackend)
+    )
+  }
+}
