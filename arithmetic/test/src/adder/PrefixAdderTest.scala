58fd5fb9cb38f060f90069b84f16dbf39057f87a
01xz <73350430+01xz@users.noreply.github.com>
2023-11-11 12:57:47 +0800

    test(adder): add more test cases

--- arithmetic/test/src/adder/PrefixAdderTest.scala
+++ arithmetic/test/src/adder/PrefixAdderTest.scala
@@ -9,4 +9,13 @@ class PrefixAdderTest extends AnyFlatSpec with ChiselScalatestTester with Formal
   "Test Han Carlson Prefix Adder 8" should "PASS" taggedAs FormalTag in {
     verify(new HanCarlsonPrefixAdder(8), Seq(BoundedCheck(1), DefaultBackend))
   }
+  "Test Han Carlson Prefix Adder 16" should "PASS" taggedAs FormalTag in {
+    verify(new HanCarlsonPrefixAdder(16), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Han Carlson Prefix Adder 32" should "PASS" taggedAs FormalTag in {
+    verify(new HanCarlsonPrefixAdder(32), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Han Carlson Prefix Adder 64" should "PASS" taggedAs FormalTag in {
+    verify(new HanCarlsonPrefixAdder(64), Seq(BoundedCheck(1), DefaultBackend))
+  }
 }
