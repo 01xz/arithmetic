2c544652c4a1742b4fdbe9b41e3cce1f9fcc6643
01xz <73350430+01xz@users.noreply.github.com>
2023-11-14 16:31:21 +0800

    test(adder): add more test cases

--- arithmetic/test/src/adder/PrefixAdderTest.scala
+++ arithmetic/test/src/adder/PrefixAdderTest.scala
@@ -6,6 +6,45 @@ import chiseltest.formal._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class PrefixAdderTest extends AnyFlatSpec with ChiselScalatestTester with Formal with FormalBackendOption {
+  "Test Ladner Fischer Prefix Adder 8" should "PASS" taggedAs FormalTag in {
+    verify(new LadnerFischerPrefixAdder(8), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Ladner Fischer Prefix Adder 16" should "PASS" taggedAs FormalTag in {
+    verify(new LadnerFischerPrefixAdder(16), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Ladner Fischer Prefix Adder 32" should "PASS" taggedAs FormalTag in {
+    verify(new LadnerFischerPrefixAdder(32), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Ladner Fischer Prefix Adder 64" should "PASS" taggedAs FormalTag in {
+    verify(new LadnerFischerPrefixAdder(64), Seq(BoundedCheck(1), DefaultBackend))
+  }
+
+  "Test Kogge Stone Prefix Adder 8" should "PASS" taggedAs FormalTag in {
+    verify(new KoggeStonePrefixAdder(8), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Kogge Stone Prefix Adder 16" should "PASS" taggedAs FormalTag in {
+    verify(new KoggeStonePrefixAdder(16), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Kogge Stone Prefix Adder 32" should "PASS" taggedAs FormalTag in {
+    verify(new KoggeStonePrefixAdder(32), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Kogge Stone Prefix Adder 64" should "PASS" taggedAs FormalTag in {
+    verify(new KoggeStonePrefixAdder(64), Seq(BoundedCheck(1), DefaultBackend))
+  }
+
+  "Test Brent Kung Prefix Adder 8" should "PASS" taggedAs FormalTag in {
+    verify(new BrentKungPrefixAdder(8), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Brent Kung Prefix Adder 16" should "PASS" taggedAs FormalTag in {
+    verify(new BrentKungPrefixAdder(16), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Brent Kung Prefix Adder 32" should "PASS" taggedAs FormalTag in {
+    verify(new BrentKungPrefixAdder(32), Seq(BoundedCheck(1), DefaultBackend))
+  }
+  "Test Brent Kung Prefix Adder 64" should "PASS" taggedAs FormalTag in {
+    verify(new BrentKungPrefixAdder(64), Seq(BoundedCheck(1), DefaultBackend))
+  }
+
   "Test Han Carlson Prefix Adder 8" should "PASS" taggedAs FormalTag in {
     verify(new HanCarlsonPrefixAdder(8), Seq(BoundedCheck(1), DefaultBackend))
   }
