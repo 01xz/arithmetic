ba0be1e4a65b81f55a440338c1b515cd21522916
01xz <73350430+01xz@users.noreply.github.com>
2023-11-16 10:15:33 +0800

    docs(adder): docs of abstract class `Adder`

--- arithmetic/src/adder/Adder.scala
+++ arithmetic/src/adder/Adder.scala
@@ -3,6 +3,10 @@ package arithmetic.adder
 import chisel3._
 import chisel3.util.Cat
 
+/** An abstract class representing an adder module.
+  *
+  * @param width The bit width of the adder.
+  */
 abstract class Adder(width: Int) extends Module {
   val lhs, rhs = IO(Input(UInt(width.W)))
   val cin      = IO(Input(Bool()))
