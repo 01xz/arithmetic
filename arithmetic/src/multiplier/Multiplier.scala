1d2221576e9a86274aa5429b4308eab7df486f40
01xz <73350430+01xz@users.noreply.github.com>
2023-11-16 10:12:59 +0800

    fix(multiplier): abstract `BoothMultiplier` rather than `BoothWallaceMultiplier`

--- arithmetic/src/multiplier/Multiplier.scala
+++ arithmetic/src/multiplier/Multiplier.scala
@@ -2,8 +2,7 @@ package arithmetic.multiplier
 
 import chisel3._
 import chisel3.util.Cat
-import arithmetic.compressor.WallaceTree
-import arithmetic.compressor.Compressor
+import arithmetic.compressor._
 import arithmetic.util.SignExt
 
 abstract class Multiplier[T <: Data](in: => T, out: => T) extends Module {
@@ -11,15 +10,11 @@ abstract class Multiplier[T <: Data](in: => T, out: => T) extends Module {
   val product  = IO(Output(out))
 }
 
-abstract class BoothWallaceMultiplier(width: Int)
-    extends Multiplier(UInt(width.W), UInt())
-    with Booth
-    with WallaceTree
-    with Compressor {
+abstract class BoothMultiplier(width: Int) extends Multiplier(UInt(width.W), UInt()) with Booth with CompressorTree {
   val ls, rs = IO(Input(Bool()))
 
   val pp = getPartialProductOf(SignExt(ls, lhs), SignExt(rs, rhs))
   product := getTwoOperandsOf(pp).reduce(_ + _)(width * 2 - 1, 0)
 }
 
-class Radix4BoothWallaceMultiplier(width: Int) extends BoothWallaceMultiplier(width) with Radix4Booth
+class Radix4BoothWallaceMultiplier(width: Int) extends BoothMultiplier(width) with Radix4Booth with WallaceTree
