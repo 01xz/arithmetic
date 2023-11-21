6bf42b9f608e9d182024b0f361c3892397a84fff
01xz <73350430+01xz@users.noreply.github.com>
2023-11-21 21:45:19 +0800

    fix(mac): additional `sub` signal for add/sub control

--- arithmetic/src/mac/MAC.scala
+++ arithmetic/src/mac/MAC.scala
@@ -2,12 +2,13 @@ package arithmetic.mac
 
 import chisel3._
 
-/** An abstract Multiply-Accumulator (MAC) module.
+/** An abstract Multiply-Accumulator (MAC) module to calculate: s +/- (a * b).
   * @param mWidth The bit width of the signals to be multiplied.
   * @param aWidth The bit width of the signal to accumulated.
   */
 abstract class MAC(mWidth: Int, aWidth: Int) extends Module {
   val a, b   = IO(Input(UInt(mWidth.W)))
   val s      = IO(Input(UInt(aWidth.W)))
+  val sub    = IO(Input(Bool()))
   val result = IO(Output(UInt()))
 }
